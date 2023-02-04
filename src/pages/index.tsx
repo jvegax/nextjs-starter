28015ccfd492f23d27297f21f2e1e109d334e37c
jvegax <toledo.utrera@hotmail.com>
2023-02-04 17:31:08 +0100

    chore: cofig

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 
-
-const Home = () => (
-  <div>
-    <h1>Game Trail 🎮</h1>
-  </div>
-);
+const Home = () => {
+  return (
+    <div>
+      <h1>Game Trail 🎮</h1>
+    </div>
+  );
+};
 
 export default Home;
